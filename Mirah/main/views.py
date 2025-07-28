71b43ed7f3b35b92452175303959cf1d70948e95
sarah <sarah@sarahs-MacBook-Pro.local>
2025-07-28 18:23:16 +0300

    Update homepage design,apply final UI tweaks, and revise README content

--- Mirah/main/views.py
+++ Mirah/main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
 def home_view(request:HttpRequest):
     return render(request,"main/index.html")
+
+def about_view(request:HttpRequest):
+    return render(request,"main/about.html")
 
